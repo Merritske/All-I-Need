7f93710598d28f0f7e25d793e5367e4baca4fb96
Merritske <merritske@gmail.com>
2022-01-30 11:26:30 +0100

    update 30 11

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ let tbody = document.querySelector("tbody")
 let rekensom = document.querySelector(".rekensom")
 let som = document.querySelector("#som")
 let checkresultBtn = document.querySelector("#uitkomst")
+let cT = document.querySelector("#correct")
 //invoeren username
 //pop-up rekensom met random getallen
 //resultaat
@@ -27,23 +28,41 @@ let checkresultBtn = document.querySelector("#uitkomst")
 userBtn.addEventListener("click", adUser=>{
     adUser.preventDefault()
 userN.innerHTML = userI.value
+
 userI.value = ""
-modal.style.display = "block"
 
 let a = Math.floor(Math.random()*10)
 let b = Math.floor(Math.random()*10)
 let c = a+b
 rekensom.innerHTML = `${a} + ${b} =`
+
 checkresultBtn.addEventListener("click", resultaat=>{
   resultaat.preventDefault()
-  console.log("biep")
-  console.log(c)
-  console.log(som.value)
+
+  modal.style.visibility = "visible"
   if(som.value == c){
        // matrix(scaleX(),skewY(),skewX(),scaleY(),translateX(),translateY())
         cirkel.style.transform = "matrix(0, 0.5, 0.5, 0, 1400,0 )"
 vierk.style.transform = "matrix(0, 5, 0.5, 0, 900, 100)"
-  
+let correctT = "correct!"
+let arT = []
+  for(let i of correctT){
+   arT.push(i)
+  }
+  console.log(arT)
+     
+let x=0
+  let show =  setInterval(function(){  
+  cT.innerHTML += arT[x]
+ console.log(arT[x]) 
+cT.style.color ="red"
+
+x ++
+ if(x == 8){
+   clearInterval(show)
+ }
+} , 600) 
+   
 }else{
 
   vierk.style.transform = "matrix(0, 3, 0.5, 0, 900, 100)"
@@ -51,4 +70,5 @@ vierk.style.transform = "matrix(0, 5, 0.5, 0, 900, 100)"
   alert(`helaas je slaat de bal helemaal mis, de juiste oplossing is ${c}`)
 }
 })
+
 })
