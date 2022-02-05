cb9b55fcdd01104a1db7698386e177afe20ad5e9
Merritske <merritske@gmail.com>
2022-02-05 12:58:43 +0100

    update userName nt ok

--- app.js
+++ app.js
@@ -29,7 +29,6 @@ let cT = document.querySelector("#correct")
 //resultaat
 //wordt in een lijst gezet
 
-  userI.value = ""
 //nieuwe gebruiker maken
 userBtn.addEventListener("click", adUser => {
   adUser.preventDefault()
@@ -46,26 +45,21 @@ userBtn.addEventListener("click", adUser => {
 
 })
 
-window.addEventListener("DOMContentLoaded", ()=>{
+window.addEventListener("DOMContentLoaded", () => {
 
   db.collection("rekengame").onSnapshot((snapshot) => {
-
-tb.innerHTML =""
+    userI.innerHTML = ""
+    tb.innerHTML = ""
     snapshot.forEach((doc) => {
-  
-     listItem.push({
+
+      listItem.push({
         id: doc.id,
         ...doc.data()
       })
-      console.log(doc.data())
+     // console.log(doc.data())
 
-
-
-
-
-
-userI.innerHTML += `<option value=${doc.data().user}>${doc.data().user}</option>`
-     tb.innerHTML += `<tr>
+      userI.innerHTML += `<option value=${doc.data().user}>${doc.data().user}</option>`
+      tb.innerHTML += `<tr>
       <th id="${doc.data().id}">
        ${doc.data().user}
       </th>
@@ -78,100 +72,140 @@ userI.innerHTML += `<option value=${doc.data().user}>${doc.data().user}</option>
       <td >${doc.data().percentage} %</td>
   </tr>`
 
-    })  
+    })
 
 
 
-  
-  // console.log(listItem)
+
+    // console.log(listItem)
+
+  })
 
 })
 
-})
-
-  let countCorrect = 0
-  let aantal = 0
-  let percentage = countCorrect / aantal * 100
+let countCorrect = 0
+let aantal = 0
+let percentage = countCorrect / aantal * 100
 
 
- let listItem = []
+let listItem = []
 db.collection("rekengame").onSnapshot((snapshot) => {
 
 
   snapshot.forEach((doc) => {
 
-   listItem.push({
+    listItem.push({
       id: doc.id,
       ...doc.data()
     })
- 
- 
-// tb.innerHTML =""
 
 
-  userI.innerHTML += `<option value=${doc.data().user}>${doc.data().user}</option>`
-//      tb.innerHTML += `<tr>
-  //     <th id="${doc.data().id}">
-  //      ${doc.data().user}
-  //     </th>
-  // </tr>
-  
-  // <tr>
-  // <td></td>
-  //     <td >${doc.data().aantal}</td>
-  
-  //     <td >${doc.data().percentage} %</td>
-  // </tr>`
-  
-//   // console.log(listItem)
+    // tb.innerHTML =""
 
-   })
+
+    //  userI.innerHTML += `<option value=${doc.data().user}>${doc.data().user}</option>`
+    //      tb.innerHTML += `<tr>
+    //     <th id="${doc.data().id}">
+    //      ${doc.data().user}
+    //     </th>
+    // </tr>
+
+    // <tr>
+    // <td></td>
+    //     <td >${doc.data().aantal}</td>
+
+    //     <td >${doc.data().percentage} %</td>
+    // </tr>`
+
+    //   // console.log(listItem)
+
+  })
 
 
 
   goBtn.addEventListener("click", openReken => {
 
 
-    console.log(listItem)
+   // console.log(listItem)
 
     if (confirm(`Is ${userI.value} your name?`)) {
 
       reken.style.visibility = "visible"
       let a = Math.floor(Math.random() * 10)
       let b = Math.floor(Math.random() * 10)
-      let c = a + b
+    
       rekensom.innerHTML = `${a} + ${b} =`
+let c;
+      let opgave = Math.floor(Math.random() * (0 - 4) + 4)
+     // console.log(opgave)
+      switch (opgave) {
+        case 0:
+          c = a + b
+          rekensom.innerHTML = `${a} + ${b} =`;
+          break;
+        case 1:
+          if(a<b || a ==0){
+             c = b -a
+             break;
+           } else {
+               c = a - b
+               
+           } 
+         
+          rekensom.innerHTML = `${a} - ${b} =`;
+          break;
+        case 2:
+          c = a * b
+          rekensom.innerHTML = `${a} * ${b} =`
+          break;
+        case 3:
+          if(a<b || a ==0){
+            console.log(a)
+            c = b /a
+              break;
+          }  else{
+               c = a / b
+             
+          }
+       
+          rekensom.innerHTML = `${a} : ${b} =`
+          break;
+      }
+
+
       userN.innerHTML = userI.value
 
       checkresultBtn.addEventListener("click", resultaat => {
         resultaat.preventDefault()
-
-        console.log(countCorrect)
+userI.value =  userN.innerHTML
+      //  console.log(countCorrect)
         modal.style.visibility = "visible"
         if (som.value == c) {
-      
-console.log(userN.innerHTML)
+
+          console.log(userI.value)
+          userI.innerHTML = ""
+          userI.innerHTML = userI.value
 
           for (let u = 0; u < listItem.length; u++) {
-           if (listItem[u].user == userN.innerHTML) {
+            if (listItem[u].user == userN.innerHTML) {
 
-            countCorrect = listItem[u].countCorrect +1
-            console.log(countCorrect)
-            aantal = listItem[u].aantal + 1
-            percentage = Math.floor(countCorrect / aantal * 100)
-          
+              countCorrect = listItem[u].countCorrect + 1
+             // console.log(countCorrect)
+              aantal = listItem[u].aantal + 1
+              percentage = Math.floor(countCorrect / aantal * 100)
+
               id = listItem[u].id
               db.collection("rekengame").doc(id).update({
                 percentage: percentage,
                 aantal: aantal,
                 countCorrect: countCorrect
               })
-         
+
             }
-       
+
           }
-        
-          console.log(countCorrect)
+
+        //  console.log(countCorrect)
 
           //move om aan te duiden dat het correct is + "correct dat verschijnt"
           // matrix(scaleX(),skewY(),skewX(),scaleY(),translateX(),translateY())
@@ -182,21 +216,27 @@ console.log(userN.innerHTML)
           for (let i of correctT) {
             arT.push(i)
           }
-          console.log(arT)
+         // console.log(arT)
           let x = 0
           let show = setInterval(function () {
             cT.innerHTML += arT[x]
-            console.log(arT[x])
+           // console.log(arT[x])
             cT.style.color = "red"
             x++
             if (x == 12) {
               clearInterval(show)
               //nog niet goed, user zou moeten blijven maar de rest zou moeten gereload zijn -> ook bij "else" veranderen
-              confirm("play again?") ? location.reload() : location.reload();
+             
+          if(confirm("play again?")){
+location.reload();
+
+          }else{
+            location.reload()
+          }  
             }
           }, 600)
 
- 
+
 
         } else {
           let correctT = "Helaas, je slaat de bal mis!           "
@@ -204,11 +244,11 @@ console.log(userN.innerHTML)
           for (let i of correctT) {
             arT.push(i)
           }
-          console.log(arT)
+        //  console.log(arT)
           let x = 0
           let show = setInterval(function () {
             cT.innerHTML += arT[x]
-            console.log(arT[x])
+           // console.log(arT[x])
             cT.style.color = "red"
 
             x++
@@ -219,10 +259,10 @@ console.log(userN.innerHTML)
             }
           }, 300)
           vierk.style.transform = "matrix(0, 3, 0.5, 0, 900, 100)"
-    
+
           for (let u = 0; u < listItem.length; u++) {
-countCorrect = listItem[u].countCorrect
-            console.log(countCorrect)
+            countCorrect = listItem[u].countCorrect
+           // console.log(countCorrect)
             aantal = listItem[u].aantal + 1
             percentage = Math.floor(countCorrect / aantal * 100)
 
@@ -234,12 +274,12 @@ countCorrect = listItem[u].countCorrect
                 percentage: percentage,
                 aantal: aantal,
                 countCorrect: countCorrect
-              }) 
-                  
+              })
+
             }
-       
-          } 
-     
+
+          }
+
         }
 
 
@@ -251,7 +291,7 @@ countCorrect = listItem[u].countCorrect
       alert("select your name or create a username")
 
     }
-   
+
 
   })
 
