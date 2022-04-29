849c9292c5c0acbf63c7f3cc98ea0944d4cfda9f
An <93704845+Merritske@users.noreply.github.com>
2022-04-29 13:45:42 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,12 @@
 // Your web app's Firebase configuration
         // For Firebase JS SDK v7.20.0 and later, measurementId is optional
         const firebaseConfig = {
-            apiKey: "AIzaSyB6U4BVAYcgOm8clrBoe8cpAzVFIhniN6E",
-            authDomain: "todolist-a60b5.firebaseapp.com",
-            projectId: "todolist-a60b5",
-            storageBucket: "todolist-a60b5.appspot.com",
-            messagingSenderId: "279568936226",
-            appId: "1:279568936226:web:4031f449e1351780f1097b",
-            measurementId: "G-1FN0WBFPT3"
+            apiKey: "AIzaSyCdA-lwasFmQlvPYGRa3jLWsVGCM1J20ZE",
+  authDomain: "mytodolist-59b8a.firebaseapp.com",
+  projectId: "mytodolist-59b8a",
+  storageBucket: "mytodolist-59b8a.appspot.com",
+  messagingSenderId: "863281685464",
+  appId: "1:863281685464:web:62f71046406ed5574c08ad"
           };
           firebase.initializeApp(firebaseConfig);
         
