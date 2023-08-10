17b9c785c8b10d84a847eeeb6ab46d4988be3869
merritske <merritske@gmail.com>
2023-08-10 21:19:29 +0200

    dropdownmenu ok

---
+++ hso/index.js
@@ -0,0 +1,5 @@
+const submenu =document.getElementsByClassName("submenu")
+// submenu.forEach(element => {element.adEventListener('onmouseenter', console.log('piep'))
+    
+// }); 
+console.log(submenu)
