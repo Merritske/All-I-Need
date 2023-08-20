b41d69085324ee1d471b5f1be03930e76de1a32f
merritske <merritske@gmail.com>
2023-08-20 20:45:00 +0200

    concerten

--- hso/index.js
+++ hso/index.js
@@ -3,3 +3,43 @@ const submenu =document.getElementsByClassName("submenu")
     
 // }); 
 console.log(submenu)
+
+let modal= document.getElementsByClassName("concert")
+console.log(modal[0].children)
+for(x=0;x<modal.length;x++){
+    console.log(modal[x])
+    modal[x].addEventListener('click',
+    function(e){
+ 
+             console.log(e)
+        })
+      
+  
+}
+// //MODAL
+// // Get the modal
+ var popup = document.getElementById("myModal");
+
+// // Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// // Get the <span> element that closes the modal
+ var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks on the button, open the modal
+btn.onclick = function() {
+  popup.style.display = "block";
+ }
+
+// // When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  popup.style.display = "none";
+ }
+
+// // When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+   if (event.target == modal) {
+    modal.style.display = "none";
+   }
+ }
+
