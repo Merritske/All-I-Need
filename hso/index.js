87873ca3e6088413b754b0819d8664c34c36f5a7
merritske <merritske@gmail.com>
2023-08-23 23:31:29 +0200

    popup concerten ok

--- hso/index.js
+++ hso/index.js
@@ -4,42 +4,46 @@ const submenu =document.getElementsByClassName("submenu")
 // }); 
 console.log(submenu)
 
-let modal= document.getElementsByClassName("concert")
-console.log(modal[0].children)
-for(x=0;x<modal.length;x++){
-    console.log(modal[x])
-    modal[x].addEventListener('click',
-    function(e){
- 
-             console.log(e)
-        })
-      
-  
-}
+
 // //MODAL
 // // Get the modal
- var popup = document.getElementById("myModal");
-
+ var popup = document.getElementsByClassName("fotos");
+console.log(popup)
 // // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
+var btn = document.getElementsByClassName('myBtn')
+console.log(btn[0])
 // // Get the <span> element that closes the modal
- var span = document.getElementsByClassName("close")[0];
+ var span = document.getElementsByClassName("close");
 
 // // When the user clicks on the button, open the modal
-btn.onclick = function() {
-  popup.style.display = "block";
+
+      for(x=0; x<btn.length; x++){
+    
+          console.log(x)
+    
+ btn[x].onclick = function() {
+const el = event.currentTarget
+    console.log(el.nextElementSibling)
+ 
+el.nextElementSibling.style.display = "block"; 
+
  }
 
+}
+
+console.log(span.parentElement)
+
 // // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  popup.style.display = "none";
+for(x=0; x<span.length; x++)
+span[x].onclick = function() {
+const el = event.currentTarget
+el.parentElement.style.display = "none";
  }
 
 // // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-   if (event.target == modal) {
-    modal.style.display = "none";
-   }
- }
+// window.onclick = function(event) {
+//    if (event.target == popup) {
+//     popup.style.display = "none";
+//    }
+//  }
 
